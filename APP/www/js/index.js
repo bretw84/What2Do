cb2acd35239caa059a704df0496244684d884efb
bretw84 <bretw84@gmail.com>
2019-02-26 07:14:00 -0600

    working on program - committing progress - need to figure out reorder

--- APP/www/js/index.js
+++ APP/www/js/index.js
@@ -1,175 +1,218 @@
 var app = {
-    // Application Constructor
+
     initialize: function() {
         this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+	},
+	
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+	},
+	
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		app.receivedEvent('deviceready');
 
-        console.log('Received Event: ' + id);
-    }
-};
-
-$(document).ready(function() {
-	
-    hidePreloader();
+		hidePreloader();
 		
-	var height = '';
-	
-	height = $(window).height();
-	
-	$('.container-fluid').css('min-height',height);	
-	        
-	//Create empty array for options to be input
-	var options=[];
-	
-	//When submit option button is clicked:
-	$('#submit').click(function(){
+		var height = '';
 		
-		//Check if input has text...
-		//if empty... do noting
-		//if not... continue the code
-		if($('#options').val() != '') {
-			
-			var count = options.length + 1;
-			
-			//Add the input field data to the #options div (for html)
-			$('#options-div').append('<div class="items">'+$('#options').val()+'</div>');
-			
-			//Add the input field data to the options array (for randomizing)
-			options.push($('#options').val());
-			
-			//Empty #options input after an #options is submitted
-			$('#options').val('');
-			
-			//Hide "No Options Yet" after at least 1 #options
-			$('#no-options').css('display','none');
-			
-			//Show Choose an options button after at least 1 #options
-			$('#choose').css('display','block');
-			
-		}
+		height = $(window).height();
 		
-	});
-	
-	//When choose an option button is click
-	//Note, we done need to check if it exists
-	//because it would be hidden if no options added
-	$('#choose').click(function() {
-        
-        showPreloader();
-		
-		//Randomize options array from inputs
-		var rand = options[Math.floor(Math.random() * options.length)];
-		
-		//Show the winning choice after choosing
-		//to pick random from inputs, css #chosen span
-		$('#chosen').html('WINNER: <div><span>'+rand+'!!!</span></div>');
-		
-		//Add border to winners circle
-		$('#chosen').css('border','1px solid #000000').css('padding','20px');
-        
-        $(this).val('CHOOSE AGAIN...');
-        
-        hidePreloader();
-		
-	});
-	
-	//When clear is clicked... start over
-	$('#clear').click(function() {
-		
-		if(options.length != 0) {
-		
-			//Confirm you want to clear everything
-			if(confirm('Are You Sure')) {
-                
-                showPreloader();
-			
-				//Empty out options array
-				options = [];
+		$('.container-fluid').css('min-height',height);	
 				
-				//Emptyu Out #options-div
-				$('#options-div').html('');
+		//Create empty array for options to be input
+		var options=[];
+
+		$("#options").keyup(function(event) {
+			if (event.keyCode === 13) {
+				$("#addItem").click();
+			}
+		});
+		
+		//When submit option button is clicked:
+		$('#addItem').click(function(){			
+
+			//Check if input has text...
+			//if empty... do noting
+			//if not... continue the code
+			if($('#options').val() != '') {
+
+				$('#clear').show();
+								
+				//Add the input field data to the #options div (for html)
+				$('#options-div').append('<div class="items">'+$('#options').val()+'</div>');
 				
-				//Empty options input
+				//Add the input field data to the options array (for randomizing)
+				options.push($('#options').val());
+				
+				//Empty #options input after an #options is submitted
 				$('#options').val('');
 				
-				//Empty out #chosen div
-				$('#chosen').html('');
+				//Hide "No Options Yet" after at least 1 #options
+				$('#no-options').css('display','none');
 				
-				//Hide Choose option button
-				$('#choose').css('display','none');
-                
-                //Reset choose button text
-                $('#choose').val('MAKE A CHOICE!!!');
+				//Show Choose an options button after at least 1 #options
+				$('#choose').css('display','block');
+				$('#randomize').css('display','block');
 				
-				//Show No Options Selected text
-				$('#no-options').css('display','block');
-				
-				//Hide border&padding from #chosen option div
-				$('#chosen').css('border','none').css('padding','0');
-                
-                hidePreloader();
-				
-			} else {}
+			}
 			
+		});
+		
+		//When choose an option button is clicked
+		//Note, we dont need to check if it exists
+		//because it would be hidden if no options added
+		$('#choose').click(function() {
+			
+			showPreloader();
+
+			$('#randomized').html('').removeClass('visible');
+			
+			//Randomize options array from inputs
+			var rand = options[Math.floor(Math.random() * options.length)];
+			
+			//Show the winning choice after choosing
+			//to pick random from inputs, css #chosen span
+			$('#chosen').html('<div class="heading">Winner:</div> <div><span>'+rand+'!!!</span></div>');
+			
+			//Add border to winners circle
+			$('#chosen').addClass('visible');
+			
+			$(this).val('Choose Again...');
+			
+			hidePreloader();
+			
+		});
+
+		//When randomize button is click
+		//Note, we dont need to check if it exists
+		//because it would be hidden if no options added
+		$('#randomize').click(function() {
+			
+			showPreloader();
+
+			$('#chosen').html('').removeClass('visible');
+			
+			//Randomize options array from inputs
+			// var rand = options[Math.floor(Math.random() * options.length)];
+
+			function shuffle(array) {
+				var currentIndex = array.length, temporaryValue, randomIndex;
+			  
+				// While there remain elements to shuffle...
+				while (0 !== currentIndex) {
+			  
+				  // Pick a remaining element...
+				  randomIndex = Math.floor(Math.random() * currentIndex);
+				  currentIndex -= 1;
+			  
+				  // And swap it with the current element.
+				  temporaryValue = array[currentIndex];
+				  array[currentIndex] = array[randomIndex];
+				  array[randomIndex] = temporaryValue;
+				}
+			  
+				return array;
+y
+			}
+
+			var rand = shuffle(options);
+			console.log(rand);
+			var html = '';
+			rand.each = function() {
+				html += rand;
+			}
+
+			//Show the winning choice after choosing
+			//to pick random from inputs, css #chosen span
+			$('#randomized').html('<div class="heading">Order:</div><span>'+html+'</span>');
+			
+			//Add border to winners circle
+			$('#randomized').addClass('visible');
+			
+			$(this).val('Randomize Again...');
+			
+			hidePreloader();
+			
+		});
+		
+		//When clear is clicked... start over
+		$('#clear').click(function() {
+			
+			if(options.length != 0) {
+			
+				//Confirm you want to clear everything
+				if(confirm('Are You Sure')) {
+					
+					showPreloader();
+
+					$('#clear').hide();
+				
+					//Empty out options array
+					options = [];
+					
+					//Emptyu Out #options-div
+					$('#options-div').html('');
+					
+					//Empty options input
+					$('#options').val('');
+					
+					//Empty out #chosen div
+					$('#chosen').html('');
+					$('#randomized').html('');
+					
+					//Hide Choose option button
+					$('#choose').css('display','none');
+					$('#randomize').css('display','none');
+					
+					//Reset choose button text
+					$('#choose').val('Choose One');
+					$('#randomize').val('Randomize Options');
+					
+					//Show No Options Selected text
+					$('#no-options').css('display','block');
+					
+					//Hide border&padding from #chosen option div
+					$('#chosen').html('').removeClass('visible');
+					$('#randomized').html('').removeClass('visible');
+					
+					hidePreloader();
+					
+				} else {}
+				
+			}
+			
+		});
+
+		$(window).resize(function() {
+				
+			var height = '';
+			
+			height = $(window).height();
+			
+			$('.container-fluid').css('min-height',height);		
+			
+		});
+		
+		function showPreloader() {
+			"use strict";
+			$('body').css({'overflow':'hidden'});
+			$('#preloader').show(); // will fade out the white DIV that covers the website. 
+			$('#status').show(); // will first fade out the loading animation 
 		}
 		
-	});
-	
-	$('.fa-trash').click(function() {
-	
-		"use strict";
-		
-		var id = $(this).attr('id');
-		console.log($(this).attr('id'));
-				
-	});
-	
-});
+		function hidePreloader() {
+			"use strict";
+			$('#status').delay(1000).fadeOut(); // will first fade out the loading animation 
+			$('#preloader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+			$('body').delay(1000).css({'overflow':'visible'});
+		}
 
-$(window).resize(function() {
+	},
 	
-	"use strict";
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+	}
 	
-	var height = '';
-	
-	height = $(window).height();
-	
-	$('.container-fluid').css('min-height',height);		
-	
-});
-
-function showPreloader() {
-	"use strict";
-	$('body').css({'overflow':'hidden'});
-	$('#preloader').show(); // will fade out the white DIV that covers the website. 
-	$('#status').show(); // will first fade out the loading animation 
-}
-
-function hidePreloader() {
-	"use strict";
-	$('#status').delay(1000).fadeOut(); // will first fade out the loading animation 
-	$('#preloader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-	$('body').delay(1000).css({'overflow':'visible'});
-}
+};
